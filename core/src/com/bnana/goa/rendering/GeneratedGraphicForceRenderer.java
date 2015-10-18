69293adc9cd4c9d4331ccf4aed95a9cce722854d
lucapiccinelli <luca.picci@gmail.com>
2015-10-18 18:06:22 +0200

    rimosso tween non più necessario sul rendering del campo di forza

--- core/src/com/bnana/goa/rendering/GeneratedGraphicForceRenderer.java
+++ core/src/com/bnana/goa/rendering/GeneratedGraphicForceRenderer.java
@@ -5,29 +5,16 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Array;
 import com.bnana.goa.force.ForceField;
 import com.bnana.goa.tween.PercentageManager;
 import com.bnana.goa.tween.ShapeRendererAccessor;
 import com.bnana.goa.utils.ScaleManager;
-
-import java.awt.geom.Point2D;
-import java.util.List;
-
-import javax.swing.SingleSelectionModel;
 
 import aurelienribon.tweenengine.BaseTween;
 import aurelienribon.tweenengine.Timeline;
 import aurelienribon.tweenengine.Tween;
 import aurelienribon.tweenengine.TweenCallback;
 import aurelienribon.tweenengine.TweenManager;
-import aurelienribon.tweenengine.equations.Back;
-import aurelienribon.tweenengine.equations.Bounce;
-import aurelienribon.tweenengine.equations.Circ;
-import aurelienribon.tweenengine.equations.Elastic;
-import aurelienribon.tweenengine.equations.Linear;
-import aurelienribon.tweenengine.equations.Quad;
-import aurelienribon.tweenengine.equations.Sine;
 
 /**
  * Created by Luca on 9/21/2015.
@@ -40,9 +27,7 @@ public class GeneratedGraphicForceRenderer implements ForceRenderer{
     private float lastMagnitude;
     private boolean setOffAnimation;
     private boolean setOnAnimation;
-    private Vector2 lastPosition;
     private TweenManager positionTweenManager;
-    private Vector2 workPosition;
 
     public GeneratedGraphicForceRenderer(ShapeRenderer shapeRenderer, ScaleManager sm) {
         this.shapeRenderer = shapeRenderer;
@@ -51,8 +36,6 @@ public class GeneratedGraphicForceRenderer implements ForceRenderer{
 
         setOffAnimation = false;
         setOnAnimation = false;
-        lastPosition = new Vector2();
-        workPosition = new Vector2();
 
         setTweenAnimation();
     }
@@ -89,7 +72,6 @@ public class GeneratedGraphicForceRenderer implements ForceRenderer{
                     Tween.to(shapeRenderer, ShapeRendererAccessor.COLOR_ALPHA, 0.3f).target(0).start(tweenManager);
                 }
                 magnitude = lastMagnitude;
-                position.set(lastPosition.x, lastPosition.y);
             }
             else {
                 if(!setOnAnimation) {
@@ -109,16 +91,7 @@ public class GeneratedGraphicForceRenderer implements ForceRenderer{
             }
         }
 
-        if(!lastPosition.equals(position)){
-            if(lastMagnitude == 0) lastPosition.set(position);
-
-            positionTweenManager.killAll();
-            workPosition.set(lastPosition);
-            Tween.to(workPosition, 0, 1f).target(position.x, position.y).start(positionTweenManager);
-        }
-
         lastMagnitude = magnitude;
-        lastPosition.set(position.x, position.y);
 
         Gdx.gl.glEnable(GL20.GL_BLEND);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
@@ -131,7 +104,7 @@ public class GeneratedGraphicForceRenderer implements ForceRenderer{
         positionTweenManager.update(Gdx.graphics.getDeltaTime());
 
         float radius = percentageManager.scale(unscaledRadius);
-        shapeRenderer.circle(workPosition.x, workPosition.y, radius, 50);
+        shapeRenderer.circle(position.x, position.y, radius, 50);
 
         shapeRenderer.end();
         Gdx.gl.glDisable(GL20.GL_BLEND);
--- core/src/com/bnana/goa/stage/OverviewStage.java
+++ core/src/com/bnana/goa/stage/OverviewStage.java
@@ -144,7 +144,6 @@ public class OverviewStage extends Stage implements ContactListener, OrganismGro
     private void createCamera() {
         this.renderer = new Box2DDebugRenderer();
         this.camera = (OrthographicCamera) getCamera();
-        //camera.setToOrtho(false, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
         camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0f);
         camera.update();
 
