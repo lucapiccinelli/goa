950b8cbf99a10ec3c5851426dfe94f1167fb52f4
lucapiccinelli <luca.picci@gmail.com>
2015-11-10 08:01:49 +0100

    prova di disegno della convex hull dell'organismo

--- core/src/com/bnana/goa/rendering/FlatGeneratedGraphicOrganismRenderer.java
+++ core/src/com/bnana/goa/rendering/FlatGeneratedGraphicOrganismRenderer.java
@@ -1,9 +1,11 @@
 package com.bnana.goa.rendering;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.ConvexHull;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.FloatArray;
 import com.bnana.goa.cell.Cell;
 import com.bnana.goa.cell.OffCell;
 import com.bnana.goa.organism.Organism;
@@ -17,18 +19,21 @@ import java.util.Comparator;
 public class FlatGeneratedGraphicOrganismRenderer implements OrganismRenderer {
     private final ShapeRenderer shapeRenderer;
     private final ScaleManager sm;
-    private Array<Vector2> positions;
+    private FloatArray positions;
     private PositionInfo[] borderPoints;
     private final int borderQuantizationFactor;
     private int borderQuantizedDimension;
+    private final ConvexHull convexHull;
 
     public FlatGeneratedGraphicOrganismRenderer(ShapeRenderer shapeRenderer, ScaleManager sm) {
         this.shapeRenderer = shapeRenderer;
         this.sm = sm;
-        this.positions = new Array<Vector2>();
+        this.positions = new FloatArray();
 
-        borderQuantizationFactor = 20;
-        borderQuantizedDimension = 360 / 20;
+        convexHull = new ConvexHull();
+
+        borderQuantizationFactor = 30;
+        borderQuantizedDimension = 360 / borderQuantizationFactor;
         borderPoints = new PositionInfo[borderQuantizedDimension];
     }
 
@@ -37,45 +42,58 @@ public class FlatGeneratedGraphicOrganismRenderer implements OrganismRenderer {
         positions.clear();
         organism.use(this);
 
-        borderQuantizedDimension = Math.min(borderQuantizedDimension, positions.size);
-
-        Vector2 avg = new Vector2(0, 0);
+        /*Vector2 avg = new Vector2(0, 0);
         for (Vector2 position : positions) {
             avg.add(position);
         }
         avg.scl(1f / positions.size);
 
+        PositionInfo currentInfo;
         for (Vector2 position : positions) {
             float distance = position.dst(avg);
-            float alpha = distance > 0 ? (MathUtils.radiansToDegrees * MathUtils.atan2(position.y - avg.y, position.x - avg.x) + 180) % 360 : 0;
-            int alphaToInt = (int)alpha / borderQuantizationFactor;
+            if(distance > 0){
+                float alpha = (MathUtils.radiansToDegrees * MathUtils.atan2(position.y - avg.y, position.x - avg.x) + 180) % 360;
+                int alphaToInt = (int)alpha / borderQuantizationFactor;
 
-            PositionInfo currentInfo = borderPoints[alphaToInt];
-            if (currentInfo == null){
-                borderPoints[alphaToInt] = new PositionInfo(position, alpha, distance);
-            }else {
-                if(currentInfo.getDistance() < distance){
-                    currentInfo.setAlpha(alpha);
-                    currentInfo.setDistance(distance);
-                    currentInfo.setPosition(position);
+                currentInfo = borderPoints[alphaToInt];
+                if (currentInfo == null){
+                    borderPoints[alphaToInt] = new PositionInfo(position, alpha, distance);
+                }else {
+                    if(distance > currentInfo.getDistance()){
+                        currentInfo.setAlpha(alpha);
+                        currentInfo.setDistance(distance);
+                        currentInfo.setPosition(position);
+                    }
                 }
+            }
+        }*/
+
+        FloatArray polygon = convexHull.computePolygon(positions, false);
+
+        shapeRenderer.setColor(0, 0, 0, 0);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+
+        shapeRenderer.polygon(polygon.items);
+
+        /*for (int i = 0; i < borderQuantizedDimension; i++){
+            currentInfo = borderPoints[i];
+
+            if(currentInfo != null) {
+                Vector2 position = currentInfo.getPosition();
+                shapeRenderer.circle(position.x, position.y, 0.5f);
             }
         }
 
-        shapeRenderer.setColor(0, 0, 0, 0);
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-
-        for (int i = 0; i < borderQuantizedDimension; i++){
-            Vector2 position = borderPoints[i].getPosition();
-            shapeRenderer.circle(position.x, position.y, 0.5f);
-        }
+        shapeRenderer.setColor(1, 1, 0, 0);
+        shapeRenderer.circle(avg.x, avg.y, 0.5f);*/
 
         shapeRenderer.end();
     }
 
     @Override
     public void use(Cell cell, Vector2 position, float density) {
-        positions.add(position);
+        positions.add(position.x);
+        positions.add(position.y);
     }
 
     @Override
